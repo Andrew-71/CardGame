b003073929c171137f8d858bca5f590c2e188e0b
Andrew-71 <andrey.nikitin.vladimirovich@gmail.com>
2023-03-26 19:28:02 +0300

    Minor JSON additions

--- Assets/Scripts/LevelManagement.cs
+++ Assets/Scripts/LevelManagement.cs
@@ -2,23 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: Move all of *gestures broadly* this into its own file!
+
+[Serializable]
+public class Category
+{
+    public string name;
+    public Level[] levels;
+
+    int size() {
+        return levels.Length;
+    }
+}
+
+[Serializable]
+public class Hint
+{
+    public int cost;
+    public string type;
+    public string content;
+}
+
+[Serializable]
+class Level
+{
+    public string[] answer;
+    public Hint[] hints;
+    bool guessed;
+    int last_guessed;
+}
+
 public class LevelManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextAsset categoriesFolder;
+    [Serializable] private 
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
+    void updateCategories()
+    {
+        Category category = JsonUtility.FromJson<Category>(jsonFile.text);  // TODO: Yes, this just loads a single category.
+    }
+
     void GetCategoryStats(string name) 
     {
-        
+
     }
 
     void LoadLevel(string filename)
